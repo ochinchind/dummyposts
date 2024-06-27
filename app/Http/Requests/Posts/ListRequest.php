a6cedb397b42c0b609a09c893cabd3bf0c286585
ochinchind <amanrahim93@gmail.com>
2024-06-27 10:30:49 +0500

    no authorize in list request

--- app/Http/Requests/Posts/ListRequest.php
+++ app/Http/Requests/Posts/ListRequest.php
@@ -9,16 +9,6 @@ use Illuminate\Http\Exceptions\HttpResponseException;
 class ListRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
      * @return array
